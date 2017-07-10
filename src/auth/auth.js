6c8097893b3d8c80f75c76c27619b13e3bc3613a
Terry <knight3001@aliyun.com>
2017-07-10 12:34:32 +1000

    password hash

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -7,10 +7,36 @@ import {
     Redirect,
     withRouter
 } from 'react-router-dom';
+import { hashSync, genSaltSync, compareSync } from 'bcryptjs';
 
+import genSalt from './salt'
 import { BaseUrl } from '../globals';
 
 const LoginUrl = BaseUrl + "login/";
+
+
+let server = {
+    init() {
+        let admin = 'terry';
+        let adminSalt = genSalt(admin);
+        let adminPass = hashSync('admin', adminSalt);
+        let users = {
+            [admin]: adminPass
+        }
+        return users;
+    },
+
+    check(username, password) {
+        const users = this.init();
+        if (users[username] !== undefined && compareSync(password, users[username])) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
 
 const fakeAuth = {
     isAuthenticated: false,
@@ -61,23 +87,26 @@ export class Login extends Component {
         this.handleChange = this.handleChange.bind(this);
     }
 
-    login = () => {
+    login = (e, prevState) => {
+        e.preventDefault();
         const username = this.state.username;
         const password = this.state.password;
-        if (username === "terry" && password === "admin") {
+
+        if (server.check(username, password)) {
             fakeAuth.authenticate(() => {
-                this.setState({ redirectToReferrer: true })
+                this.setState({ redirectToReferrer: true });
             })
         }
-        else{
-            this.setState({ error: true })
+        else {
+            this.setState({ error: true });
         }
     }
 
     handleChange() {
         this.setState({
             username: this.username.value,
-            password: this.password.value
+            password: this.password.value,
+            error: false
         })
     }
 
@@ -94,23 +123,28 @@ export class Login extends Component {
         return (
             <div className="row">
                 <div className="col-xs-12 col-sm-12 col-md-4 col-md-offset-4">
-                    <form className="form-horizontal form-wrap">
+                    <form className="form-horizontal form-wrap" onSubmit={this.login}>
                         <fieldset>
-                            <div className={(this.state.error? "form-group has-error" : "form-group")}>
+                            {this.state.error &&
+                                <div className="form-group">
+                                    <span className="form-error col-md-8 col-md-offset-2">Wrong Combination</span>
+                                </div>
+                            }
+                            <div className={(this.state.error ? "form-group has-error" : "form-group")}>
                                 <label htmlFor="username" className="col-md-3 col-md-offset-1 control-label">Username</label>
                                 <div className="input-group col-md-5">
                                     <input type="text" className="form-control" id="username" placeholder="terry" maxLength="50"
                                         value={this.state.username} ref={(input) => this.username = input} onChange={this.handleChange} />
                                 </div>
                             </div>
-                            <div className={(this.state.error? "form-group has-error" : "form-group")}>
+                            <div className={(this.state.error ? "form-group has-error" : "form-group")}>
                                 <label htmlFor="password" className="col-md-3 col-md-offset-1 control-label">Username</label>
                                 <div className="input-group col-md-5">
                                     <input type="password" className="form-control" id="password" placeholder="admin" maxLength="50"
                                         value={this.state.password} ref={(input) => this.password = input} onChange={this.handleChange} />
                                 </div>
                             </div>
-                            <button type="button" className="btn btn-primary" onClick={this.login}>Log in</button>
+                            <button type="submit" className="btn btn-primary">Log in</button>
                         </fieldset>
                     </form>
                 </div>
--- src/auth/salt.js
+++ src/auth/salt.js
@@ -4,7 +4,7 @@ import btoa from 'btoa'
  * Generate 16 bytes salt for bcrypt by seed. Should return the same salt for the same seed.
  * @param  {string} seed The seed for salt
  */
-export default function (seed) {
+export default function genSalt(seed) {
     let bytes = []
 
     for (let i = 0, l = seed.length; i < l; i++) {
